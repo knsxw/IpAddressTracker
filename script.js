61fcc529a0fe169431c1adc9bbac1bfd5d7bd08a
knsxw <letsmail.khinekhant@gmail.com>
2024-08-19 22:54:24 +0700

    ipCheckerValid

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ const searchForm = document.querySelector(".header_form");
 searchForm.addEventListener("submit", (e) => {
     e.preventDefault();
     const value = document.querySelector("#search").value;
-    searchIpAddress(value);
+    if (checkUserInput(value)) {
+        searchIpAddress(value);
+    } else {
+        alert("Please Enter Valid IP Address!!");
+    }
 });
 async function searchIpAddress(ipAddress) {
     const apiKey = "at_nt3WeqWuEfFMyEzoFSax4uuU9i9H9";
@@ -40,6 +44,27 @@ function createMap(lat, lng, country, region) {
     L.marker([lat, lng], {icon: myIcon}).addTo(map)
     	.bindPopup(`${region}, ${country}`)
     	.openPopup();
+}
+const checkUserInput = (value) => {
+    const parts = value.split('.');
+    // IPv4 address should have exactly 4 parts
+    if (parts.length !== 4) {
+        return false;
+    }
+
+    for (let i = 0; i < parts.length; i++) {
+        const part = parts[i];
+        // Each part should be a number
+        if (!/^\d+$/.test(part)) {
+            return false;
+        }
+        const num = parseInt(part, 10);
+        // Each number should be between 0 and 255
+        if (num < 0 || num > 255) {
+            return false;
+        }
+    }
+    return true;
 }
 const defaultIp = "26.37.52.179";
 searchIpAddress(defaultIp)
